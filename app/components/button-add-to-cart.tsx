0a9ee36eb6a848417868ec7282e280e14ffbc7fd
Mathis Beauville <mathisbeauville@Mathiss-MacBook-Pro.local>
2025-01-03 22:31:40 +0100

    feat: add product list & cart state

---
+++ app/components/button-add-to-cart.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useCartStore } from "@/app/hooks/cart";
+import { Button } from "@radix-ui/themes";
+import { useState } from "react";
+
+export function AddToCart({ id }: { id: string }) {
+    const { add } = useCartStore();
+    const [isAdding, setIsAdding] = useState(false);
+    const onAdd = () => {
+        if (isAdding) return;
+        setIsAdding(true);
+        add(id, 1);
+        setTimeout(() => setIsAdding(false), 3000);
+    };
+
+    return (
+        <Button
+            size="1"
+            variant="soft"
+            color={isAdding ? "green" : "blue"}
+            className="!mt-auto !w-auto !self-start"
+            onClick={onAdd}
+        >
+            {isAdding ? "Ajouté" : "Ajouter au panier"}
+        </Button>
+    );
+}
---
+++ app/home/page.tsx
@@ -0,0 +1,18 @@
+"use server";
+
+import { ProductCard } from "@/app/components/product-card";
+import prisma from "@/lib/prisma";
+
+export default async function Home() {
+    const products = await prisma.product.findMany();
+
+    return (
+        <div className="bg-blue-100">
+            <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 lg:grid-cols-3">
+                {products.map((product) => (
+                    <ProductCard product={product} key={product.id} />
+                ))}
+            </div>
+        </div>
+    );
+}
