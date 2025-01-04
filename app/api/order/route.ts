1632a8f06f38fd53f75a5180b82daa4147363d87
Mathis Beauville <mathis.beauville@gmail.com>
2025-01-04 14:00:48 +0100

    feat: add payment connection in front :)

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -8,56 +8,59 @@ export const POST = auth(async (req, res) => {
     let user, items;
     try {
         user = req.auth!.user;
-        items = createOrder.parse(req.body).items;
+        items = createOrder.parse(await req.json()).items;
     } catch (error: any) {
         return handleValidationError(error);
     }
 
-    // Calculate price & create stripe session
-    const products = await Promise.all(
-        items.map((item) => prisma.product.findUniqueOrThrow({ where: { id: item.id } })),
-    );
+    try {
+        let url;
 
-    await prisma.$transaction(async (prisma) => {
-        const orderCreated = await prisma.order.create({
-            data: {
-                userId: user.id,
-                consumptions: {
-                    createMany: {
-                        data: items.map((item) => ({
-                            productId: item.id,
-                            quantity: item.quantity,
-                        })),
-                        skipDuplicates: true,
+        await prisma.$transaction(async (tx) => {
+            const orderCreated = await tx.order.create({
+                data: {
+                    userId: user.id,
+                    consumptions: {
+                        createMany: {
+                            data: items.map((item) => ({
+                                productId: item.id,
+                                quantity: item.quantity,
+                            })),
+                            skipDuplicates: true,
+                        },
+                    },
+                    products: {
+                        createMany: {
+                            data: items.map((item) => ({
+                                productId: item.id,
+                                quantity: item.quantity,
+                            })),
+                            skipDuplicates: true,
+                        },
                     },
                 },
-                products: {
-                    createMany: {
-                        data: items.map((item) => ({
-                            productId: item.id,
-                            quantity: item.quantity,
-                        })),
-                        skipDuplicates: true,
+            });
+
+            const session = await Stripe.createCheckoutSession(user, orderCreated, tx);
+
+            await tx.order.update({
+                where: { id: orderCreated.id },
+                data: {
+                    Transaction: {
+                        create: {
+                            amount: session.amount,
+                            status: "PENDING",
+                            checkoutSessionId: session.sessionId,
+                        },
                     },
                 },
-            },
+            });
+
+            url = session.url;
         });
 
-        const session = await Stripe.createCheckoutSession(user, orderCreated);
-
-        await prisma.order.update({
-            where: { id: orderCreated.id },
-            data: {
-                Transaction: {
-                    create: {
-                        amount: session.amount,
-                        status: "PENDING",
-                        checkoutSessionId: session.sessionId,
-                    },
-                },
-            },
-        });
-
-        return Response.json({ url: session.url });
-    });
+        return Response.json({ url });
+    } catch (error: any) {
+        return Response.json({ error: error.message }, { status: 500 });
+    }
 });
--- app/panier/page.tsx
+++ app/panier/page.tsx
@@ -5,23 +5,48 @@ import { useFetch } from "@/app/hooks/use-fetch";
 import { displayablePrice } from "@/lib/utils";
 import { Product } from "@prisma/client";
 import { Button, Text } from "@radix-ui/themes";
+import { useState } from "react";
 
 export default function PanierPage() {
+    const [creatingOrder, setCreatingOrder] = useState(false);
     const { data, error, loading } = useFetch<{ mappedBody: Product[] }>("/api/product");
     const { get, remove } = useCartStore();
-    const productsCart = get().map((item) => {
-        const product = data?.mappedBody.find((el) => el.id === item.id);
-        if (!product) return;
+    const productsCart = get()
+        .map((item) => {
+            const product = data?.mappedBody.find((el) => el.id === item.id);
+            if (!product) return;
 
-        return {
-            id: product.id,
-            title: product.title,
-            priceMember: product.priceMember,
-            priceExternal: product.priceExternal,
-            image: product.image,
-            quantity: item.quantity,
-        };
-    });
+            return {
+                id: product.id,
+                title: product.title,
+                priceMember: product.priceMember,
+                priceExternal: product.priceExternal,
+                image: product.image,
+                quantity: item.quantity,
+            };
+        })
+        .filter((item) => !!item);
+
+    const createOrder = async () => {
+        setCreatingOrder(true);
+        const response = await fetch("/api/order", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                items: get(),
+            }),
+        });
+
+        if (!response.ok) {
+            console.error("Error creating order", response);
+            return;
+        }
+
+        const data = await response.json();
+        window.location.href = data.url;
+    };
 
     if (loading) return <div>Chargement...</div>;
     if (error) return <div>Erreur: {error.message}</div>;
@@ -31,13 +56,20 @@ export default function PanierPage() {
         <div className="flex min-h-screen justify-center bg-blue-100">
             <div className="mx-6 flex w-full flex-col gap-3 pt-3 sm:w-[500px]">
                 {productsCart.map((product) => {
-                    if (!product) return;
-
                     return (
                         <div
                             key={product.id}
-                            className="flex justify-between rounded-md bg-white p-4"
+                            className="relative flex justify-between rounded-md bg-white p-4"
                         >
+                            <Button
+                                className="!absolute !right-0 !top-0"
+                                variant="surface"
+                                size="1"
+                                color="crimson"
+                                onClick={() => remove(product.id)}
+                            >
+                                X
+                            </Button>
                             <div className="flex flex-col">
                                 <Text size="3" truncate>
                                     {product.title}
@@ -50,13 +82,36 @@ export default function PanierPage() {
                                 <Text size="3" weight="bold" className="mr-3">
                                     {displayablePrice(product.priceMember * product.quantity)}€
                                 </Text>
-                                <Button color="red" size="1" onClick={() => remove(product.id)}>
-                                    X
-                                </Button>
                             </div>
                         </div>
                     );
                 })}
+
+                <div className="flex justify-between rounded-md bg-white p-4">
+                    <Text size="3" weight="bold">
+                        Total
+                    </Text>
+                    <Text size="3" weight="bold">
+                        {displayablePrice(
+                            productsCart.reduce(
+                                (acc, item) => acc + item.priceMember * item.quantity,
+                                0,
+                            ),
+                        )}
+                        €
+                    </Text>
+                </div>
+
+                <div className="flex justify-center">
+                    <Button
+                        size="3"
+                        className="!w-fit"
+                        onClick={createOrder}
+                        loading={creatingOrder}
+                    >
+                        Commander
+                    </Button>
+                </div>
             </div>
         </div>
     );
