d38e9f155dd8f7e2fd7d0bcf670aa209664e0959
Mathis Beauville <mathis.beauville@gmail.com>
2025-01-12 13:27:32 +0100

    fix: customized price

--- app/components/product-card.tsx
+++ app/components/product-card.tsx
@@ -1,4 +1,6 @@
 import { AddToCart } from "@/app/components/button-add-to-cart";
+import { server_getUser } from "@/lib/auth";
+import { Stripe } from "@/lib/stripe";
 import { displayablePrice } from "@/lib/utils";
 import { Product } from "@prisma/client";
 import { Strong, Text } from "@radix-ui/themes";
@@ -7,7 +9,12 @@ interface ProductCardProps {
     product: Product;
 }
 
-export function ProductCard({ product }: ProductCardProps) {
+export async function ProductCard({ product }: ProductCardProps) {
+    const user = await server_getUser();
+    if (!user) {
+        return null;
+    }
+
     return (
         <>
             <div className="m-3 flex max-w-[600px] gap-3 rounded-md bg-white p-5">
@@ -19,7 +26,10 @@ export function ProductCard({ product }: ProductCardProps) {
                 />
                 <div className="flex flex-col gap-2">
                     <Text>
-                        {product.title} - <Strong>{displayablePrice(product.priceMember)}€</Strong>
+                        {product.title} -{" "}
+                        <Strong>
+                            {displayablePrice(Stripe.getCustomizedPrice(user, product))}€
+                        </Strong>
                     </Text>
                     {product.description && <Text size="1">{product.description}</Text>}
                     <AddToCart id={product.id} />
